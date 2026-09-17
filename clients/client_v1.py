eeafbfc02b662279b62863b64555d49aa4faad70
Harshit Rai <raiharshit66@gmail.com>
2026-09-17 21:59:25 +0530

    fix:Fix the fastmcp path and server dict

--- clients/client_v1.py
+++ clients/client_v1.py
@@ -1,20 +1,23 @@
 import asyncio
 from langchain_mcp_adapters.client import MultiServerMCPClient
 
-servers =  {
-    "local_expense_server" : {
-        "transport" : "stdio",
-        "command" : "D:\\hr\\my-mcp-servers\\.venv\\Scripts\\fastmcp.exe",
+servers = {
+    "local_expense_server": {
+        "transport": "stdio",
+        "command": r"D:\hr\my-mcp-servers\.venv\Scripts\fastmcp.exe",
         "args": [
             "run",
-            "D:\\hr\\my-mcp-servers\\local-mcp\\expense-tracking-mcp\\main.py"
-        ]
+            r"D:\hr\my-mcp-servers\local-mcp\expense-tracking-mcp\main.py"
+        ],
     }
 }
 
 async def main():
     print("Starting MultiServerMCPClient...")
-    client = MultiServerMCPClient(servers=servers)
+    client = MultiServerMCPClient(servers)
+    tools = await client.get_tools()
+    print("Tools retrieved from servers:")
+    print(tools)
 
 if __name__ == "__main__":
     asyncio.run(main())
